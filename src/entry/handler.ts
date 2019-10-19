a0dece5631844684a0034e6ead1abb37dfc9b0a5
ajinkya <ajinkya_awchat@yahoo.com>
2019-10-18 17:36:02 -0700

    Update

--- src/entry/handler.ts
+++ src/entry/handler.ts
@@ -1,15 +1,23 @@
-module.exports.handler = (event: any, context: any): any => {
-    console.log(event);
-    console.log(context);
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Go Serverless v1.0! Your function executed successfully!',
-                input: event,
-            },
-            null,
-            2
-        ),
-    };
+import { ApiRequest } from '../models/lambda';
+
+
+module.exports.handler = (event: ApiRequest, context: any): any => {
+    try {
+        console.log(event.httpMethod);
+
+        return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    }
+    catch(err) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: 'Go Serverless v1.0! Your function executed successfully!',
+                    input: event,
+                },
+                null,
+                2
+            ),
+        };
+    }
 };
--- src/repositories/DoctorRepository.ts
+++ src/repositories/DoctorRepository.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import * as _ from "lodash";
 import { Doctor } from "../models/doctor";
 
 export class DoctorRepository {

dd7ac9a97d392da3ab2462ff29058a959e39ee79
ajinkya <ajinkya_awchat@yahoo.com>
2019-10-18 17:17:15 -0700

    Added appointment service

--- src/services/DoctorService.ts
+++ src/services/DoctorService.ts
@@ -1,8 +1,24 @@
+import { DoctorRepository } from "../repositories/DoctorRepository";
 
 export class DoctorService {
     constructor(private doctorRepo: DoctorRepository) {}
 
-    public async listDoctors() {
-        
+    public async listDoctors(): Promise<string> {
+        const results = await this.doctorRepo.list();
+        if(results.length === 0) {
+            throw new Error('No doctors were found in the system yet');
+        }
+        return JSON.stringify(results);
+    }
+
+    public async getDoctorById(id: number): Promise<string> {
+        if(id < 1) {
+            throw new Error(`Invalid: No doctor with id ${id} was found in the system yet`);
+        } 
+        const doctor = await this.doctorRepo.get(id);
+        if(!doctor) {
+            throw new Error(`No doctor with id ${id} was found in the system yet`);
+        }
+        return JSON.stringify(doctor);
     }
 }
